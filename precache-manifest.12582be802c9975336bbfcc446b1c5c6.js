ed48dfc017fbe29a0cfc44ca105a1b07d4c5d72e
nChe <nasstya.chernyshova@gmail.com>
2018-12-07 11:07:52 +0200

    Updates

---
+++ precache-manifest.12582be802c9975336bbfcc446b1c5c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eabb8a1aafc5c14e9db1",
+    "url": "/empeek-test/static/js/runtime~main.eabb8a1a.js"
+  },
+  {
+    "revision": "afb7e01c8a288a00cf06",
+    "url": "/empeek-test/static/js/main.afb7e01c.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/empeek-test/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "afb7e01c8a288a00cf06",
+    "url": "/empeek-test/static/css/main.54b68688.chunk.css"
+  },
+  {
+    "revision": "e8977d8179b6076303aadd6d04e87298",
+    "url": "/empeek-test/index.html"
+  }
+];
